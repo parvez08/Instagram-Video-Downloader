b72722185ebabf6a518f259b7be5794175d591ab
Parvez Shaikh 2 <parvezshaikh.0894@gmail.com>
2023-08-17 09:48:46 +0530

    - Added id and isSent variables in model.

--- app/src/main/java/com/example/instagramvideodownloader/work_manager/models/SmsMessageModel.kt
+++ app/src/main/java/com/example/instagramvideodownloader/work_manager/models/SmsMessageModel.kt
@@ -1,7 +1,9 @@
 package com.example.instagramvideodownloader.work_manager.models
 
 data class SmsMessageModel(
+    val id: String,
     val body: String,
     val address: String,
-    val date: Long
+    val date: Long,
+    val isSent: Boolean
 )
